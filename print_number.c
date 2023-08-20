0eff2c92c423af99c3d1c373762962b042231748
A7mdos <a7mdos1999@gmail.com>
2023-08-21 01:18:14 +0200

    Updated print_number function

--- print_number.c
+++ print_number.c
@@ -9,21 +9,21 @@
  */
 int print_number(int num)
 {
-	unsigned int number = num;
-	static int printed_chars_count;
+	unsigned int number = num, printed_chars_count = 0;
 
 	if (num < 0)
 	{
 		_putchar('-');
 		printed_chars_count++;
-		number *= -1;
+		number = -number;
 	}
 
-	if (number / 10 > 0)
-		print_number(number / 10);
+	if ((number / 10) > 0)
+	{
+		printed_chars_count += print_number(number / 10);
+	}
 
-	_putchar('0' + (number % 10));
-	printed_chars_count++;
+	_putchar((number % 10) + '0');
 
-	return (printed_chars_count);
+	return (printed_chars_count + 1);
 }
