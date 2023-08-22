b2d567609d7306f1a2a59f6d65dfee9eeb2b5bc3
monatagelsir7 <monatagelsir777@gmail.com>
2023-08-22 14:01:53 -0700

    percentage

---
+++ tests/2-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+	int len;
+	int len2;
+
+	len = _printf("%b", 1023);
+	_putchar('\n');
+    len2 = _printf("%b", UINT_MAX - 1);
+	_putchar('\n');
+
+	 len = _printf("Percent:[%%]\n");
+     len2 = printf("Percent:[%%]\n");
+
+	// len = _printf("Percent:[%%]\n");
+	// len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+
+	return (0);
+}
