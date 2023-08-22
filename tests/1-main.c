bcb62b895e08c494a2339106634289c02802f5b2
monatagelsir7 <monatagelsir777@gmail.com>
2023-08-22 13:56:51 -0700

    printing a simple sentence

--- tests/1-main.c
+++ tests/1-main.c
@@ -5,10 +5,10 @@ int main(void)
 {
 	int len;
 	int len2;
-	// len = _printf("Let's try to printf a simple sentence.\n");
-	// len2 = printf("Let's try to printf a simple sentence.\n");
-	// _printf("Length:[%d, %i]\n", len, len);
-	// printf("Length:[%d, %i]\n", len2, len2);
+	 len = _printf("Let's try to printf a simple sentence.\n");
+	 len2 = printf("Let's try to printf a simple sentence.\n");
+	 _printf("Length:[%d, %i]\n", len, len);
+	 printf("Length:[%d, %i]\n", len2, len2);
 	len = _printf("Negative:[%d]\n", -762534);
 	len2 = printf("Negative:[%d]\n", -762534);
 	_printf("Len:[%d]\n", len);
