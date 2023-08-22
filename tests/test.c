4904441cbbceaf8047c05021c0e11443f60409db
monatagelsir7 <monatagelsir777@gmail.com>
2023-08-22 14:05:38 -0700

    test files

---
+++ tests/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - test the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count;
+
+	count = print_number(-123);
+	_putchar('\n');
+	printf("%d\n", count);
+
+	return (0);
+}
+
