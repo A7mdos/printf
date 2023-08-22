5df8d898a4e86f286bfab6ba32bae3f38f73fbc2
A7mdos <a7mdos1999@gmail.com>
2023-08-22 16:21:45 +0200

    Using the buffer, by some of the functions

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@
 
 int _printf(const char *format, ...)
 {
-	int i, printed_chars_count = 0;
+	int i, buffer_chars_count = 0, added_chars_count = 0;
 	int (*print)(va_list, char *);
 	va_list args;
 	char *buffer, *buffer_ptr;
@@ -30,23 +30,24 @@ int _printf(const char *format, ...)
 				return (-1);
 
 			print = get_print_func(format + (i + 1));
-
 			if (print == NULL)
-			{
 				return (-1);
-			}
 
-			printed_chars_count += print(args, buffer);
+			added_chars_count += print(args, buffer);
+			buffer += added_chars_count;
+			buffer_chars_count += added_chars_count;
 			i++;
 		}
 		else
 		{
-			_putchar(format[i]);
-			printed_chars_count++;
+			*buffer = format[i];
+			buffer++;
+			buffer_chars_count++;
 		}
 	}
-
+	*buffer = '\0';
+	write(1, buffer_ptr, buffer_chars_count);
 	va_end(args);
 	free(buffer_ptr);
-	return (printed_chars_count);
+	return (buffer_chars_count);
 }
--- print_functions.c
+++ print_functions.c
@@ -13,7 +13,7 @@ int print_c(va_list arg, char *buffer)
 	char letter;
 
 	letter = va_arg(arg, int);
-	_putchar(letter);
+	*buffer = letter;
 
 	return (1);
 }
@@ -39,7 +39,8 @@ int print_s(va_list arg, char *buffer)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		*buffer = str[i];
+		buffer++;
 		printed_chars_count++;
 	}
 
@@ -59,7 +60,7 @@ int print_percent(va_list arg, char *buffer)
 {
 	UNUSED(arg);
 
-	_putchar('%');
+	*buffer = '%';
 
 	return (1);
 }
