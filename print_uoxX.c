488c229c068be14ba13272c343a846806afbb676
A7mdos <a7mdos1999@gmail.com>
2023-08-22 19:13:23 +0200

    Revert "A check for numbers that could be considered integers - print_u has some issues"
    This reverts commit d3d3b651d96133f91b3e166f01ffb18bf380476e.

--- print_uoxX.c
+++ print_uoxX.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * print_u - Prints an unsigned integer.
@@ -15,12 +14,6 @@ int print_u(va_list arg, char *buffer)
 	int half1_digits_count = 1, half2_digits_count = 1;
 
 	number = va_arg(arg, unsigned int);
-
-	if (number <= INT_MAX)
-	{
-		arg -= sizeof(unsigned int);
-		return(print_di(arg, buffer));
-	}
 
 	number_half1 = number / 10000;
 	number_copy = number_half1;
